49660772680ae8848e78cdf100fd665680ec2391
Abiaz <torstenrahmann@googlemail.com>
2022-08-18 15:09:15 +0200

    Auto Login

--- rpa.py
+++ rpa.py
@@ -39,20 +39,30 @@ def logout():
 
 def login_abiaz():
 
-    rpa.wait_and_click(['btn_login_menu.png'])
+   rpa.wait_and_click(['btn_login_email.png'])
+
+     if rpa.end_of_queue_state():
+         rpa.wait_and_click(['btn_login_abiaz.png'])
 
     if rpa.end_of_queue_state():
-        rpa.wait_and_click(['btn_login_logout.png'])
+        rpa.wait_and_click(['btn_login_play.png'])
 
-        rpa.sleep(10)
-       
-    
-        
-        #if rpa.end_of_queue_state():
-         #   rpa.wait_and_click(['btn_login_email.png'])
+def login_floki():
 
-          #  if rpa.end_of_queue_state():
-           #     rpa.wait_and_click(['btn_login_abiaz.png'])
+   rpa.wait_and_click(['btn_login_email.png'])
+
+     if rpa.end_of_queue_state():
+         rpa.wait_and_click(['btn_login_floki.png'])
+
+    if rpa.end_of_queue_state():
+        rpa.wait_and_click(['btn_login_play.png'])
+
+def login_fearil():
+
+   rpa.wait_and_click(['btn_login_email.png'])
+
+     if rpa.end_of_queue_state():
+         rpa.wait_and_click(['btn_login_fearil.png'])
 
     if rpa.end_of_queue_state():
         rpa.wait_and_click(['btn_login_play.png'])
