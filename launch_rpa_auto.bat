7c449ee508687cd2817d58cbc055446564f13ce0
Abiaz <torstenrahmann@googlemail.com>
2022-08-22 08:51:18 +0200

    Added shutdown

--- launch_rpa_auto.bat
+++ launch_rpa_auto.bat
@@ -1,5 +1,6 @@
 @REM Launch RPA client
 SET RPA_LOGLEVEL=INFO
-C:\Users\torst\Abiaz\rpa
+cd C:\Users\torst\Abiaz\rpa
 pipenv run python rpa.py --ini-file rpa.ini --profile RPA_auto
-Pause
+shutdown -s
+
