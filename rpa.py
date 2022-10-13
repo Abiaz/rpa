f22fa394e8b44818c337d2a7e05feeb97f6932f0
Abiaz <torstenrahmann@googlemail.com>
2022-10-13 08:39:49 +0200

    Spooky arena

--- rpa.py
+++ rpa.py
@@ -268,7 +268,7 @@ def arena():
     Step: 1st fight in arena
     ----------------------------------------------------------------------
     """
-    rpa.wait_and_click(['btn_arena.png', 'btn_arena2.png'])
+    rpa.wait_and_click(['btn_arena.png', 'btn_arena2.png','btn_arena3.png'])
 
     rpa.save_queue('start_loop')
 
