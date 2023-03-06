cb66320896f35cbc519abff31dfd5b6823bcc3c9
Abiaz <torstenrahmann@googlemail.com>
2023-03-06 08:43:35 +0100

    rpa.ini

--- rpa.py
+++ rpa.py
@@ -129,6 +129,16 @@ def login_sonne():
         if rpa.end_of_queue_state():
             rpa.wait_and_click(['btn_login_play.png'])
 
+def login_agr():
+
+    rpa.wait_and_click(['btn_login_email.png'])
+
+    if rpa.end_of_queue_state():
+        rpa.wait_and_click(['btn_login_agr.png'])
+
+        if rpa.end_of_queue_state():
+            rpa.wait_and_click(['btn_login_play.png'])
+
 def login_luiz():
 
     rpa.wait_and_click(['btn_login_email.png'])
